921a2f24171b9dfa5bf35ec9c6d973b5985a14cf
Adithya Swaroop <thisisadithya11@gmail.com>
2021-07-18 20:05:17 +0530

    Create reverse_nodes_in_k_group.py

---
+++ Easy/reverse_nodes_in_k_group.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/explore/challenge/card/july-leetcoding-challenge-2021/610/week-3-july-15th-july-21st/3818/
+
+class Solution:
+    def reverseKGroup(self, head, k):
+        dummy = ListNode(0)
+        dummy.next = head
+        cur, nxt, pre = dummy, dummy, dummy
+        cnt = 0
+        while cur.next:
+            cnt += 1
+            cur = cur.next
+            
+        while cnt >= k:
+            cur = new = pre.next
+            nxt = cur.next
+            for _ in range(k-1):
+                tmp = nxt.next
+                nxt.next = cur
+                cur = nxt
+                nxt = tmp
+            
+            pre.next = cur
+            new.next = nxt
+            pre = new
+            cnt -= k
+            
+        return dummy.next
+            
